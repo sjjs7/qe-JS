d6e4b4a1ae20a76651a95c549fb200ecb960b67b
wmfarmer <wmfarmer@mcmaster.ca>
2018-07-16 11:35:01 -0400

    Modification of LEM example.

--- Constructions/LEM_Example.ml
+++ Constructions/LEM_Example.ml
@@ -1,5 +1,21 @@
-let test = prove(`isExprType (x:epsilon) (TyBase "bool") ==> ((eval x to bool) \/ ~(eval x to bool))`,
+(* Law of excluded middle (LEM) *)
+
+let lem = prove(`!x:epsilon. isExprType x (TyBase "bool") ==> ((eval x to bool) \/ ~(eval x to bool))`,
+GEN_TAC THEN
 DISCH_TAC THEN
 ASM_REWRITE_TAC[EXCLUDED_MIDDLE]
 );;
-INST [`QuoConst "T" (TyBase "bool")`,`x:epsilon`] test;;
+
+INST [`x:epsilon`,`x:epsilon`] lem;;
+
+INST [`y:epsilon`,`x:epsilon`] lem;;
+
+INST [`QuoConst "T" (TyBase "bool")`,`x:epsilon`] lem;;
+
+INST [`QuoConst "F" (TyBase "bool")`,`x:epsilon`] lem;;
+
+INST [`Q_ (0 = 0) _Q`,`x:epsilon`] lem;;
+
+
+
+
